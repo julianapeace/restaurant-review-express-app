226887ad4cef80b52231682afac5ee21934bf34d
julianapeace <chancecordelia@gmail.com>
2017-11-06 16:21:33 -0600

    added hacky login functionality

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-var express = require('express');
-var app = express();
-var pgp = require('pg-promise')({});
-var db = pgp({database: 'restaurantv2'});
-
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')({});
+const db = pgp({database: 'restaurantv2'});
+const session = require('express-session');
+const morgan = require('morgan');
 const body_parser = require('body-parser');
-app.use(body_parser.urlencoded({extended: false}));
+
 app.set('view engine', 'hbs');
+app.use(morgan('dev'));
+app.use(body_parser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.render('index.hbs');
+app.use(session({
+  secret: process.env.SECRET_KEY || 'dev',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {maxAge: 60000}
+}));
+
+app.use(function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else if (req.path == '/login') {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/login', function(req,res){
+  res.render('login.hbs');
+});
+
+app.post('/login', function(req,res, next){
+  var data = {reviewer_email : req.body.username, password: req.body.password}
+
+  var query = "SELECT EXISTS(select 1 FROM reviewer WHERE reviewer_email = ${reviewer_email} AND password = ${password})"
+  db.one(query, data)
+    .then(function(result){
+      if(result['exists']=== false){
+        console.log('user does not exist')
+        var error = "Username or password is incorrect."
+        res.render('login.hbs', {'error':error})
+      }
+      else{
+        console.log('user exists')
+        req.session.user = req.body.username; //creates secure cookie with encrypted username
+        res.redirect('/')
+      }
+    })
+    .catch(next);
+});
+
+app.get('/', function(req, res, next){
+  var user_data = {reviewer_email: req.session.user}
+  var query = 'SELECT * FROM reviewer WHERE reviewer_email = ${reviewer_email}'
+  db.one(query, user_data)
+    .then(result => {
+      let name = result['reviewer_name']
+      res.render('index.hbs', {'name':name});
+    })
+    .catch(next);
 });
 app.get('/search', function(req, res, next){
   //with GET forms, you should be getting the parameters from the query. not the body -.-;
@@ -40,7 +91,7 @@ app.post('/restaurant/submit_new', function(req, res, next){
 });
 app.get('/restaurant/:id', function(req, res, next){
   var id = req.params.id
-  var query = 'SELECT * FROM restaurant FULL JOIN review ON restaurant.id = review.restaurant_id FULL JOIN reviewer on review.reviewer_id = reviewer.id WHERE restaurant.id = $1'
+  var query = 'SELECT * FROM restaurant LEFT JOIN review ON restaurant.id = review.restaurant_id LEFT JOIN reviewer on review.reviewer_id = reviewer.id WHERE restaurant.id = $1'
   db.any(query, id)
     .then(function(result){
       console.log(result)
@@ -57,18 +108,30 @@ app.get('/restaurant/:id', function(req, res, next){
 });
 
 app.post('/addreview', function(req, res, next){
-  var data = {review_stars: req.body.stars, review_title: req.body.title, review: req.body.review, restaurant_id: req.body.restaurant_id}
-  var restaurant_id = req.body.restaurant_id
-  var restaurant_name = req.body.restaurant_name
-  var query = 'INSERT INTO review \
-  VALUES (default, NULL, ${review_stars}, ${review_title}, ${review}, ${restaurant_id})'
-console.log(data)
-  db.result(query, data)
+  //get the reviewer ID first,
+  var user_data = {reviewer_email: req.session.user}
+  var user_query = 'SELECT * FROM reviewer WHERE reviewer_email = ${reviewer_email}'
+  db.one(user_query, user_data)
     .then(function(result){
-      console.log(result)
-      res.redirect('/restaurant/'+ restaurant_id)
+      console.log(result['id'])
+      return result['id']
     })
-    .catch(next)
+    .then(function(id){
+      var data = {reviewer_id: id, review_stars: req.body.stars, review_title: req.body.title, review: req.body.review, restaurant_id: req.body.restaurant_id}
+      var restaurant_id = req.body.restaurant_id
+      var restaurant_name = req.body.restaurant_name
+      var query = 'INSERT INTO review \
+      VALUES (default, ${reviewer_id}, ${review_stars}, ${review_title}, ${review}, ${restaurant_id})'
+    console.log(data)
+      db.result(query, data)
+        .then(function(result){
+          console.log(result)
+          res.redirect('/restaurant/'+ restaurant_id)
+        })
+        .catch(next)
+    })
+    .catch(next);
+
 });
 
 app.listen(8000, function () {
